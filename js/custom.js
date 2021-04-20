f3448e3ff0a92670139915ab671ee6794b0a14f7
anna-devil-op <anna-devil-op@disnae.org>
2021-04-20 11:24:54 +0100

    Linted!

--- js/custom.js
+++ js/custom.js
@@ -2,87 +2,89 @@
 # for Jekyll parsing
 ---
 
-const contactModel = (function() {
-  let _email = '';
-  let _isValidEmail = false;
-  let _name = '';
-  let _message = '';
-  let _interests = new Set();
-  let _addToMailingList = false;
-  let _signupDetails = null;
-  let _signupError = false;
-  let _isSending = false;
+'use strict'
 
-  let _clear = () => {
-    _email = '';
-    _isValidEmail = false;
-    _name = '';
-    _message = '';
-    _interests.clear();
-    _addToMailingList = false;
+const contactModel = (function () {
+  let _email = ''
+  let _isValidEmail = false
+  let _name = ''
+  let _message = ''
+  const _interests = new Set()
+  let _addToMailingList = false
+  let _signupDetails = null
+  let _signupError = false
+  let _isSending = false
+
+  const _clear = () => {
+    _email = ''
+    _isValidEmail = false
+    _name = ''
+    _message = ''
+    _interests.clear()
+    _addToMailingList = false
   }
 
-  let setEmail = (email) => {
-    _email = email;
-    _isValidEmail = (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(_email));
+  const setEmail = (email) => {
+    _email = email
+    _isValidEmail = (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(_email))
   }
 
-  let isValidEmail = () => _isValidEmail;
+  const isValidEmail = () => _isValidEmail
 
-  let getEmail = () => _email;
+  const getEmail = () => _email
 
-  let setName = (name) => _name = name
+  const setName = (name) => { _name = name }
 
-  let getName = () => _name;
+  const getName = () => _name
 
-  let hasInterest = (interest) => _interests.has(interest);
+  const hasInterest = (interest) => _interests.has(interest)
 
-  let setInterested = (interest, interested) => { (interested) ? _interests.add(interest) : _interests.delete(interest) }
+  const setInterested = (interest, interested) => { (interested) ? _interests.add(interest) : _interests.delete(interest) }
 
-  let setAddToMailingList = (add) => _addToMailingList = add
+  const setAddToMailingList = (add) => { _addToMailingList = add }
 
-  let isAddToMailingList = () => _addToMailingList
+  const isAddToMailingList = () => _addToMailingList
 
-  let setMessage = (message) => { _message = message }
+  const setMessage = (message) => { _message = message }
 
-  let getMessage = () => _message;
+  const getMessage = () => _message
 
-  let canSubmit = () => _email !== '' && _isValidEmail && _name !== '';
+  const canSubmit = () => _email !== '' && _isValidEmail && _name !== ''
 
-  let signupSuccessful = () => !_signupError;
+  const signupSuccessful = () => !_signupError
 
-  let getSignupDetails = () => _signupDetails;
+  const getSignupDetails = () => _signupDetails
 
-  let isSending = () => _isSending;
+  const isSending = () => _isSending
 
-  let getPayload = () => {
+  const getPayload = () => {
     return {
-      "email": _email,
-      "name": _name,
-      "message": _message,
-      "interests": Array.from(_interests),
-      "addToMailingList": _addToMailingList
+      email: _email,
+      name: _name,
+      message: _message,
+      interests: Array.from(_interests),
+      addToMailingList: _addToMailingList
     }
   }
 
-  async function signup() {
-    _isSending = true;
-    _signupDetails = null;
-    _signupError = false;
-    let payload = getPayload()
+  async function signup () {
+    _isSending = true
+    _signupDetails = null
+    _signupError = false
+    const payload = getPayload()
     m.request({
-      method: "POST",
-      url: "{{ site.contacts_url }}",
-      body: payload,
+      method: 'POST',
+      url: '{{ site.contacts_url }}',
+      body: payload
     }).then(value => {
-      _signupDetails = payload;
-      _isSending = false;
-      _clear();
+      _signupDetails = payload
+      _isSending = false
+      _clear()
     }, reason => {
-      _signupError = true;
-      _isSending = false;
-      console.log(`Failed to submit contact details to the server because: ${JSON.stringify(reason)}`);
-    });
+      _signupError = true
+      _isSending = false
+      console.log(`Failed to submit contact details to the server because: ${JSON.stringify(reason)}`)
+    })
   }
 
   return {
@@ -101,115 +103,113 @@ const contactModel = (function() {
     signupSuccessful,
     getSignupDetails,
     signup,
-    isSending,
+    isSending
   }
-})();
+})()
 
 const interests = [
   'Eyesight Matters in Health',
   'Bates Method Fundamentals',
   'Somatic Seeing',
-  'One to One Sessions',
+  'One to One Sessions'
 ]
 
-const contact_app = document.getElementById('contact_app');
-const introduction = contact_app.textContent;
+const contactApp = document.getElementById('contact_app')
+const introduction = contactApp.textContent
 
-const signupComponent = (function() {
-  function viewSignupSuccess(name) {
-    return m('.contact-signup-success', ["Thank you ", m('em', name), ", your message has been sent to Anna."]);
+const signupComponent = (function () {
+  function viewSignupSuccess (name) {
+    return m('.contact-signup-success', ['Thank you ', m('em', name), ', your message has been sent to Anna.'])
   }
-  
-  function viewSignupFailure(name) {
-    return m('.contact-signup-fail', ["Sorry ", m('em', name), ", a problem was encountered and your message has not been sent to Anna."]);
+
+  function viewSignupFailure (name) {
+    return m('.contact-signup-fail', ['Sorry ', m('em', name), ', a problem was encountered and your message has not been sent to Anna.'])
   }
-  
-  function view() {
+
+  function view () {
     if (contactModel.signupSuccessful()) {
-      const signupDetails = contactModel.getSignupDetails();
+      const signupDetails = contactModel.getSignupDetails()
       if (signupDetails) {
-        return viewSignupSuccess(signupDetails.name);
+        return viewSignupSuccess(signupDetails.name)
       }
     } else {
-      return viewSignupFailure(contactModel.getName());
+      return viewSignupFailure(contactModel.getName())
     }
   }
-  
+
   return {
-    view,
+    view
   }
-})();
+})()
 
 const emailComponent = (function () {
-  function view() {
-    if (contactModel.getEmail() == '' || contactModel.isValidEmail()) {
+  function view () {
+    if (contactModel.getEmail() === '' || contactModel.isValidEmail()) {
       return m('.form-group', [
-        m('label', {for: '#contact_email'}, 'Email (required)'),
+        m('label', { for: '#contact_email' }, 'Email (required)'),
         m('input.form-control#contact_email[type="email"]', {
           oninput: e => contactModel.setEmail(e.target.value),
-          value: contactModel.getEmail(),
-        }),
-      ]);
+          value: contactModel.getEmail()
+        })
+      ])
     } else {
       return m('.form-group', [
-        m('label', {for: '#contact_email'}, [
+        m('label', { for: '#contact_email' }, [
           'Email (required)',
-          m('span.validation-error', "invalid")
+          m('span.validation-error', 'invalid')
         ]),
         m('input.form-control.invalid-input#contact_email[type="email"]', {
           oninput: e => contactModel.setEmail(e.target.value),
-          value: contactModel.getEmail(),
-        }),
-      ]);
+          value: contactModel.getEmail()
+        })
+      ])
     }
   }
 
   return {
-    view,
+    view
   }
-})();
-
+})()
 
 const sendButtonComponent = (function () {
-  function view() {
+  function view () {
     if (contactModel.isSending()) {
-      return m('.wait-spinner');
+      return m('.wait-spinner')
     } else {
       return m('.text-center', [
         m('button.btn .btn-primary .btn-xl', {
           disabled: !contactModel.canSubmit(),
           onclick: contactModel.signup
-        }, 'Send'),
-      ]);
+        }, 'Send')
+      ])
     }
   }
 
   return {
-    view,
+    view
   }
-})();
+})()
 
-
-m.mount(contact_app, {
-  view: function() {
+m.mount(contactApp, {
+  view: function () {
     return m('div', [
       m('p', introduction),
       m('fieldset.col-lg-8 offset-lg-2', [
         m('.form-group', [
-          m('label', {for: '#contact_name'}, 'Name (required)'),
+          m('label', { for: '#contact_name' }, 'Name (required)'),
           m('input.form-control#contact_name[type="text"]', {
             oninput: e => contactModel.setName(e.target.value),
-            value: contactModel.getName(),
-          }),
+            value: contactModel.getName()
+          })
         ]),
         m(emailComponent),
         m('.form-group', [
-          m('p', "If you would like to attend a course can you please write a little about what you do and why you would like to attend this course?"),
-          m('label', {for: 'contact_message'}, 'Message'),
+          m('p', 'If you would like to attend a course can you please write a little about what you do and why you would like to attend this course?'),
+          m('label', { for: 'contact_message' }, 'Message'),
           m('textarea.form-control#contact_message', {
             oninput: e => contactModel.setMessage(e.target.value),
-            value: contactModel.getMessage(),
-          }),
+            value: contactModel.getMessage()
+          })
         ]),
         m('.form-group .invisible', [
           m('label', [
@@ -219,13 +219,13 @@ m.mount(contact_app, {
                 m('label', [
                   m('input[type="checkbox"]', {
                     checked: contactModel.hasInterest(interest),
-                    oninput: e => contactModel.setInterested(interest, e.target.checked),
+                    oninput: e => contactModel.setInterested(interest, e.target.checked)
                   }),
-                  interest,
-                ]),
-              ]);
+                  interest
+                ])
+              ])
             })
-          ]),
+          ])
         ]),
         m('.form-group', [
           m('p', "Tick the following box if you'd like to be on my mailing list too. I'll let you know first about new workshops coming up and I'll never share your address with anyone. I send out emails about every couple of months so it won't be heavy on your inbox either."),
@@ -235,51 +235,51 @@ m.mount(contact_app, {
               m('label', [
                 m('input[type="checkbox"]', {
                   checked: contactModel.isAddToMailingList(),
-                  oninput: e => contactModel.setAddToMailingList(e.target.checked),
+                  oninput: e => contactModel.setAddToMailingList(e.target.checked)
                 }),
-                'I would like to be on your mailing list',
-              ]),
-            ]),
-          ]),
+                'I would like to be on your mailing list'
+              ])
+            ])
+          ])
         ]),
         m(sendButtonComponent),
-        m(signupComponent),
+        m(signupComponent)
       ])
-    ]);
+    ])
   }
-});
+})
 
-let infoButtons = document.getElementsByClassName('info-button');
-for (let infoButton of infoButtons) {
-  let interest = infoButton.id;
-  let buttonText = infoButton.innerHTML;
+const infoButtons = document.getElementsByClassName('info-button')
+for (const infoButton of infoButtons) {
+  const interest = infoButton.id
+  const buttonText = infoButton.innerHTML
   m.mount(infoButton, {
-    view: function() {
+    view: function () {
       return m('a.btn .btn-primary .btn-xl .page-scroll', {
         href: '#contact',
-        onclick: () => contactModel.setInterested(interest, true),
-      }, buttonText);
+        onclick: () => contactModel.setInterested(interest, true)
+      }, buttonText)
     }
-  });
+  })
 }
 
 (function () {
   'use strict'
 
-  $(window).on('scroll', function(event) {
-    var scrollValue = $(window).scrollTop();
+  $(window).on('scroll', function (event) {
+    const scrollValue = $(window).scrollTop()
     if (scrollValue > 100) {
-      $('.fixed-top').addClass('affix');
+      $('.fixed-top').addClass('affix')
     } else {
-      $('.fixed-top').removeClass('affix');
+      $('.fixed-top').removeClass('affix')
     }
-  });
+  })
 
   $(document).ready(function () {
-    $('.navbar-collapse a').click(function(){
-      $(".navbar-collapse").collapse('hide');
-    });
-  });
+    $('.navbar-collapse a').click(function () {
+      $('.navbar-collapse').collapse('hide')
+    })
+  })
 
   new WOW().init()
 })()
